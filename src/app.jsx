e3cc3ac412a91ff51d70963b56960b3cd1f8993a
Marine <marine.huygens@hotmail.fr>
2022-04-25 14:59:23 +0200

    correct app.jsx and css

--- src/app.jsx
+++ src/app.jsx
@@ -14,6 +14,7 @@ function App() {
         
           <Routes>
             <Route exact path="/" element={<Home />}/>
+            <Route exact path="/kasa" element={<Home />}/>
             <Route exact path="/about" element={<About />}/>
             <Route path="/logements/:id" element={<Accomodation />} />
             <Route path="/*" element={<Error />}/>
